5d9a3706770bb548b6bebaa90d3fdb34f9c9e405
EssaouidiYassine <166022030+EssaouidiYassine@users.noreply.github.com>
2024-08-16 00:05:10 +0100

    Update config.js

--- config.js
+++ config.js
@@ -9,20 +9,20 @@ global.setting = {
  }
 
 global.owner = [
-['212717457920', 'Nour', true],
-['212605784394', 'silana', false],
+['212648753294', 'Essaouidi', true],
+['212719596553', 'Yassine', true],
 ['', '', false]
 ]
 
 global.info = {
- nomerbot: '212605784394',
- pairingNumber: '212693227969',
- nameown: 'Nour',
- nomerown: '212717457920',
+ nomerbot: '212648753294',
+ pairingNumber: '212719596553',
+ nameown: 'Essaouidi',
+ nomerown: '212719596553',
  packname: 'sticker by ',
- author: 'Noureddine',
+ author: 'Essaouidi',
  namebot: 'ꜱɪʟᴀɴᴀ',
- wm: ''-'_꩜ 𝘚𝘪𝘭𝘢𝘯𝘢 𝘣𝘰𝘵 ꩜_'-'',
+ wm: ''-'_꩜ ➚ ிEssaouidi ⎙ ✓ ꩜_'-'',
  stickpack: 'Whatsapp',
  stickauth: 'ꜱɪʟᴀɴᴀ ʙᴏᴛ '
 }
@@ -32,9 +32,9 @@ global.media = {
  profil: 'https://i.ibb.co/3Fh9V6p/avatar-contact.png',
  did: 'https://telegra.ph/file/fdc1a8b08fe63520f4339.jpg',
  rules: 'https://telegra.ph/file/afcfa712bd09f4fcf027a.jpg',
- thumbnail: 'https://telegra.ph/file/161c0a22c03f7859c7599.jpg',
- thumb: 'https://telegra.ph/file/161c0a22c03f7859c7599.jpg',
- logo: 'https://telegra.ph/file/161c0a22c03f7859c7599.jpg',
+ thumbnail: 'https://telegra.ph/file/6333a8ff13109f1c8b38b.jpg',
+ thumb: 'https://telegra.ph/file/6333a8ff13109f1c8b38b.jpg',
+ logo: 'https://telegra.ph/file/e498786474b332a6e0081.jpg',
  unReg: 'https://telegra.ph/file/ef02d1fdd59082d05f08d.jpg',
  registrasi: 'https://telegra.ph/file/0169f000c9ddc7c3315ff.jpg',
  confess: 'https://telegra.ph/file/03cabea082a122abfa5be.jpg',
@@ -46,9 +46,8 @@ global.media = {
 
 // Sosmed
 global.url = {
- sig: 'https://instagram.com/noureddine_ouafy',
- sgh:  'https://github.com/noureddineouafy',
- sgc: 'https://chat.whatsapp.com/DABxracGsi5CQMd5d1N1j7'
+ sig: 'https://instagram.com/essaouidi_yassine',
+ sgh:  'https://github.com/EssaouidiYassine',
 }
 
 global.wait =` انتظر .. أنا أحاول تلبية طلبك ...`
@@ -56,7 +55,7 @@ global.wait =` انتظر .. أنا أحاول تلبية طلبك ...`
 // Info Wait
 global.msg = {
  wait: '⏱️ *Please be patient*\n\> Running command from *User*!',
- eror: '🤖*Bot Information*\n\> Sorry for the inconvenience in using *Bobiza Bot*. There was an error in the system while executing the command.'
+ eror: '🤖*Bot Information*\n\> Sorry for the inconvenience in using *Essaouidi Bot*. There was an error in the system while executing the command.'
 }
 
 global.multiplier = 69
